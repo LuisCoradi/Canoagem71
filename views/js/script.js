2731798d13cf60f472214a892aa61b133448238b
Luis Felipe Coradi e Sousa <92060245+LuisCoradi@users.noreply.github.com>
2023-10-27 23:19:19 -0300

    Add files via upload

--- views/js/script.js
+++ views/js/script.js
@@ -1,54 +1,130 @@
-/* Dropdown pesquisa */
-//////////////////////////////////////////////////////////////
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function openNav(){
+  document.getElementById("sidebarContainer").style.width = "250px"
 }
 
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
+function closeNav(){
+  document.getElementById("sidebarContainer").style.width = "0px"
+}
+
+function openHeaderDrop() {
+  let dropTrigger = document.getElementById("headerRight");
+  let dropdownContainer = document.getElementById("dropdownMenuProfile");
+
+  dropTrigger.addEventListener("click", function() {
+    if (dropdownContainer.style.display === "" || dropdownContainer.style.display === "none") {
+      dropdownContainer.style.display = "flex";
     } else {
-      a[i].style.display = "none";
+      dropdownContainer.style.display = "none";
     }
-  }
-} 
+  });
 
-//////////////////////////////////////////////////////////////
-
-//carrosel
-//////////////////////////////////////////////////////////////
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+  window.addEventListener("click", function(event) {
+    if (event.target !== dropTrigger && event.target !== dropdownContainer) {
+      dropdownContainer.style.display = "none";
+    }
+  });
 }
+///////////////////////////////////////////////////////////////////////////////////////
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+////////////////////////////////////////chamada////////////////////////////////////////
+(function () {
+  if (!localStorage.attendance) {
+      console.log('Creating attendance records...');
+      function getRandom() {
+          return (Math.random() >= 0.5);
+      }
+
+      var nameColumns = $('tbody .name-col'),
+          attendance = {};
+
+      nameColumns.each(function () {
+          var name = this.innerText;
+          attendance[name] = [];
+
+          for (var i = 0; i <= 11; i++) {
+              attendance[name].push(getRandom());
+          }
+      });
+
+      localStorage.attendance = JSON.stringify(attendance);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+}());
+
+/* STUDENT APPLICATION */
+$(function () {
+  var attendance = JSON.parse(localStorage.attendance),
+      $allMissed = $('tbody .missed-col'),
+      $allCheckboxes = $('tbody input');
+
+  // Count a student's missed days
+  function countMissing() {
+      $allMissed.each(function () {
+          var studentRow = $(this).parent('tr'),
+              dayChecks = $(studentRow).children('td').children('input'),
+              numMissed = 0;
+
+          dayChecks.each(function () {
+              if (!$(this).prop('checked')) {
+                  numMissed++;
+              }
+          });
+
+          $(this).text(numMissed);
+      });
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+
+  // Calculate and display percentage of presence
+  function calculatePercentage() {
+      $allMissed.each(function () {
+          var studentRow = $(this).parent('tr');
+          var dayChecks = studentRow.children('td').children('input');
+          var totalDays = dayChecks.length;
+          var numMissed = 0;
+
+          dayChecks.each(function () {
+              if (!$(this).prop('checked')) {
+                  numMissed++;
+              }
+          });
+
+          var percentage = ((totalDays - numMissed) / totalDays) * 100;
+          $(this).next('.percentage-col').text(percentage.toFixed(2) + "%");
+      });
+  }
+
+  // Check boxes, based on attendace records
+  $.each(attendance, function (name, days) {
+      var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr');
+      var dayChecks = $(studentRow).children('.attend-col').children('input');
+
+      dayChecks.each(function (i) {
+          $(this).prop('checked', days[i]);
+      });
+  });
+
+  // When a checkbox is clicked, update localStorage
+  $allCheckboxes.on('click', function () {
+      var studentRows = $('tbody .student');
+      var newAttendance = {};
+
+      studentRows.each(function () {
+          var name = $(this).children('.name-col').text();
+          var $allCheckboxes = $(this).children('td').children('input');
+
+          newAttendance[name] = [];
+
+          $allCheckboxes.each(function () {
+              newAttendance[name].push($(this).prop('checked'));
+          });
+      });
+
+      countMissing();
+      calculatePercentage();
+      localStorage.attendance = JSON.stringify(newAttendance);
+  });
+
+  countMissing();
+  calculatePercentage();
+}());
+///////////////////////////////////////////////////////////////////////////////////////
