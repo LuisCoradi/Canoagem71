0a73a2dedd6f5b69982b2be1fc602cd7f96b2df8
Guilherme Royer <groyerccl@gmail.com>
2023-10-28 14:37:06 -0300

    ajustes telaChamada

--- views/js/script.js
+++ views/js/script.js
@@ -18,3 +18,103 @@ function openHeaderDrop() {
   });
 
 }
+
+////////////////////////////////////////chamada////////////////////////////////////////
+(function () {
+  if (!localStorage.attendance) {
+      console.log('Creating attendance records...');
+      function getRandom() {
+          return (Math.random() >= 0.5);
+      }
+
+      var nameColumns = $('tbody .name-col'),
+          attendance = {};
+
+      nameColumns.each(function () {
+          var name = this.innerText;
+          attendance[name] = [];
+
+          for (var i = 0; i <= 11; i++) {
+              attendance[name].push(getRandom());
+          }
+      });
+
+      localStorage.attendance = JSON.stringify(attendance);
+  }
+}());
+
+$(function () {
+  var attendance = JSON.parse(localStorage.attendance),
+      $allMissed = $('tbody .missed-col'),
+      $allCheckboxes = $('tbody input');
+
+  // Count a student's missed days
+  function countMissing() {
+      $allMissed.each(function () {
+          var studentRow = $(this).parent('tr'),
+              dayChecks = $(studentRow).children('td').children('input'),
+              numMissed = 0;
+
+          dayChecks.each(function () {
+              if (!$(this).prop('checked')) {
+                  numMissed++;
+              }
+          });
+
+          $(this).text(numMissed);
+      });
+  }
+
+  // Calcula Porcentagem
+  function calculatePercentage() {
+      $allMissed.each(function () {
+          var studentRow = $(this).parent('tr');
+          var dayChecks = studentRow.children('td').children('input');
+          var totalDays = dayChecks.length;
+          var numMissed = 0;
+
+          dayChecks.each(function () {
+              if (!$(this).prop('checked')) {
+                  numMissed++;
+              }
+          });
+
+          var percentage = ((totalDays - numMissed) / totalDays) * 100;
+          $(this).next('.percentage-col').text(percentage.toFixed(2) + "%");
+      });
+  }
+
+  $.each(attendance, function (name, days) {
+      var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr');
+      var dayChecks = $(studentRow).children('.attend-col').children('input');
+
+      dayChecks.each(function (i) {
+          $(this).prop('checked', days[i]);
+      });
+  });
+
+  // Da update quando a checkbox Ã© clicada
+  $allCheckboxes.on('click', function () {
+      var studentRows = $('tbody .student');
+      var newAttendance = {};
+
+      studentRows.each(function () {
+          var name = $(this).children('.name-col').text();
+          var $allCheckboxes = $(this).children('td').children('input');
+
+          newAttendance[name] = [];
+
+          $allCheckboxes.each(function () {
+              newAttendance[name].push($(this).prop('checked'));
+          });
+      });
+
+      countMissing();
+      calculatePercentage();
+      localStorage.attendance = JSON.stringify(newAttendance);
+  });
+
+  countMissing();
+  calculatePercentage();
+}());
+///////////////////////////////////////////////////////////////////////////////////////
